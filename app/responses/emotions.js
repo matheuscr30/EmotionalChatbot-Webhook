851895109c0263cce7cdfb41279064042b74d29f
aline_abreu1 <aline.abreu@neppoadm.com.br>
2018-06-01 14:58:09 -0300

    emotions

--- app/responses/emotions.js
+++ app/responses/emotions.js
@@ -13,19 +13,22 @@ Second Index = Emotion
 let emotionsHaHaResponses = {
     0: {
         0: [
-            "Que bom que você está feliz, é a hora perfeita para fazer o exercício",
+            "Que bom que você está feliz, é a hora perfeita para fazer o exercício.",
             "É muito bom te ver alegre, adoro conversar com você. Vamos fazer o exercício agora?",
-            "Que bom que está se divertindo. Agora vamos fazer o exercício, pois sei que você é totalmente capaz"
+            "Que bom que está se divertindo. Agora vamos fazer o exercício, pois sei que você é totalmente capaz.",
+            "Gosto muito de te ver feliz, agora vamos fazer as atividades, você é muito eficiente, sei que terminará logo."
         ],
         1: [
             "Que bom que você está ficando mais feliz, é a hora perfeita para fazer o exercício",
             "É muito bom te ver alegre, adoro conversar com você. Vamos fazer o exercício agora?",
-            "É bom rir para a tristeza ir embora. Agora vamos fazer o exercício, pois sei que você é totalmente capaz"
+            "É bom rir para a tristeza ir embora. Agora vamos fazer o exercício, pois sei que você é totalmente capaz",
+            "Gosto muito de te ver feliz, nada de tristeza! Agora vamos fazer as atividades, você é muito eficiente, sei que terminará logo."
         ],
         2: [
             "Quanto mais risada, mais positivo ficamos e assim fica mais fácil fazer o exercício",
             "É muito bom te ver alegre, tente ser mais positivo. Eu sei que você consegue",
-            "É bom rir para diminuir o baixo astral. Agora vamos fazer o exercício, pois sei que você é totalmente capaz"
+            "É bom rir para diminuir o baixo astral. Agora vamos fazer o exercício, pois sei que você é totalmente capaz",
+            "Gosto muito de te ver feliz, nada de coisas negativas por aqui! Agora vamos fazer as atividades, você é muito eficiente, sei que terminará logo."
         ]
     },
     1: {
