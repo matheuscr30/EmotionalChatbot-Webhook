0c671caa56500baaf9f4be98ba6e6d45e2217022
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2018-06-19 17:29:04 -0300

    eu nao aguenteo mais

--- app/controllers/funcs.js
+++ app/controllers/funcs.js
@@ -13,10 +13,12 @@ module.exports.whatIsIt = function (application, req, res, intent, personality, 
     vals = searchText.split('?');
     searchText = vals[0];
     let response = "";
+    console.log(searchText);
 
     //'https://pt.wikipedia.org/w/api.php?action=opensearch&lang=pt-br&search=' + searchText + '&limit=1&namespace=0&format=json'
     request('https://pt.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=' +
         searchText + '&exsentences=5&exintro=1&explaintext=1', {json: true}, function (err, r, body) {
+        console.log(body['query']);
         if (err) {
             response = "Me desculpe, ocorreu algum erro inesperado";
             console.log(err);
@@ -24,7 +26,10 @@ module.exports.whatIsIt = function (application, req, res, intent, personality, 
             let pageId = Object.keys(body['query']['pages'])[0];
             let result = body['query']['pages'][pageId]['extract'];
 
-            response = (result != '') ? result : "Não consegui encontrar nada sobre " + searchText;
+            if (pageId == -1)
+                response  = "Me desculpe, ocorreu algum erro inesperado";
+            else
+                response = (result != '') ? result : "Não consegui encontrar nada sobre " + searchText;
         }
 
         res.json({
